97b850a8e43c690194562266215d9111fa5254f4
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2022-12-15 09:13:58 +0000

    API-5587: Library & plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,13 @@ object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
   
   private lazy val dependencies = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"             % "5.16.0",
-    "uk.gov.hmrc"             %% "play-ui"                                % "9.7.0-play-28",
-    "uk.gov.hmrc"             %% "govuk-template"                         % "5.72.0-play-28"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"             % "7.12.0",
+    "uk.gov.hmrc"             %% "play-ui"                                % "9.11.0-play-28",
+    "uk.gov.hmrc"             %% "govuk-template"                         % "5.78.0-play-28"
   )
 
   private lazy val testDependencies = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"                 % "5.16.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"                 % "7.12.0",
     "org.mockito"             %% "mockito-scala-scalatest"                % "1.7.1",
   ).map(_ % "test, it")
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,8 +2,8 @@
 resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.gov.uk/maven2"
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("uk.gov.hmrc"          %  "sbt-auto-build"          % "3.5.0")
+addSbtPlugin("uk.gov.hmrc"          %  "sbt-auto-build"          % "3.8.0")
 addSbtPlugin("uk.gov.hmrc"          %  "sbt-distributables"      % "2.1.0")
-addSbtPlugin("com.typesafe.play"    %  "sbt-plugin"              % "2.8.7")
+addSbtPlugin("com.typesafe.play"    %  "sbt-plugin"              % "2.8.18")
 addSbtPlugin("org.scalastyle"       %% "scalastyle-sbt-plugin"   % "1.0.0")
 
